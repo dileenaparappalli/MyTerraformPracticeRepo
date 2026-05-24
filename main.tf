44f7b19dae21eb3d8906cb5f45dea7f3d6c96fee
dileenaparappalli <47659170+dileenaparappalli@users.noreply.github.com>
2026-05-24 07:31:32 +0000

    Initial terraform demo

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
-resource "aws_iam_user" "cloud" {
-  name = "cloud-institute-user"
+resource "aws_iam_user" "iam-user" {
+
+  count = length(var.iam-user)
+  name = var.iam-user[count.index]
+  path = "/system/"
+
+  tags =var.tags
 }
